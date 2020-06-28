d8aa6b8d8b69c185f8c89bbf52b528795961d491
Egor <52937798+ZnoGouDj@users.noreply.github.com>
2020-06-29 00:44:40 +0300

    netent + pragmatic added

--- index.js
+++ index.js
@@ -18,13 +18,17 @@ function cleaner() {
     if (!x.value) { alert('Please, paste any shitty game in the text area'); return; }
     if (x.value.slice(0, 25) === 'You can play these slots:') { return; }
     if (x.value.slice(0, 35) === 'Вы можете играть в следующие слоты:') { return; }
-    if (x.value.search('softswiss') < 0 && x.value.search('mrslotty') < 0) { alert("For 'softswiss' and 'mrslotty' only"); return; }
+    if (x.value.search('softswiss') < 0 && x.value.search('pragmatic') < 0 && x.value.search('netent') < 0 && x.value.search('mrslotty') < 0) { alert("This provider's name has been forgotten. These are available: softswiss, pragmatic, netent, mrslotty."); return; }
     
 
     let clean = x.value
         .replace(/([A-Z])/g, ' $1')
         .replace(/softswiss/g, '')
         .replace(/mrslotty/g, '')
+        .replace(/pragmatic/g, '')
+        .replace(/netent/g, '')
+        .replace(/_not_mobile_html_sw/g, '')
+        .replace(/_mobile_html_sw/g, '')
         .replace(/[\[\]'":]+/g, '')
         .replace(/^./, function (str) {
             return str.toUpperCase();
@@ -41,3 +45,5 @@ function cleaner() {
 // ["mrslotty:SlotName", "mrslotty:SlotName"]
 // ["mrslotty:SlotName", "softswiss:SlotName"]
 // ["pragmatic:SlotName", "pragmatic:SlotName"]
+// ["netent:kingofslots_not_mobile_html_sw", "netent:kingofslots_mobile_html_sw"]
+
